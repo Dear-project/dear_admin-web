2f91eb17018edea86cf2d201346ae9f9659045c4
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-13 20:45:52 +0900

    fix :: design

--- src/Components/Home/banner/bannerList/style.ts
+++ src/Components/Home/banner/bannerList/style.ts
@@ -5,13 +5,11 @@ export const BannerArea = styled.div`
   flex-direction: column;
   align-items: center;
   width: 100%;
-  height: 13%;
-  gap: 25px;
+  height: 76px;
 `;
 export const Bannerinfo = styled.div`
   display: flex;
   width: 85%;
-  height: 76px;
   align-items: center;
   justify-content: space-evenly;
   border-radius: var(--Large, 12px);
