92a461fe1c6b80e41292ae4524ec8e4d267acd83
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-19 09:38:53 +0900

    fix :: query 에러 처리

---
+++ src/Components/common/error/index.tsx
@@ -0,0 +1,34 @@
+import { Component, ReactNode, ErrorInfo } from "react";
+
+interface Props {
+  children: ReactNode;
+}
+
+interface State {
+  hasError: boolean;
+}
+
+class ErrorBoundary extends Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(_: Error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error: Error, errorInfo: ErrorInfo) {
+    console.error("Uncaught error:", error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong.</div>;
+    }
+
+    return this.props.children;
+  }
+}
+
+export default ErrorBoundary;
