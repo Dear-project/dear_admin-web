5755e63049467566b8b120fda8242dd817fba611
해준 <127281057+ftery0@users.noreply.github.com>
2024-06-21 12:29:59 +0900

    feat :: declaration components

--- src/Components/Home/declaration/index.tsx
+++ src/Components/Home/declaration/index.tsx
@@ -1,8 +1,16 @@
-const Declaration = ()=>{
-    return(
-        <>
-        
-        </>
-    )
-}
+import * as S from "./style";
+import DeclarationList from "./List";
+
+const Declaration = () => {
+  return (
+    <S.Main>
+      <S.Direction>
+        <S.searchBox></S.searchBox>
+        <S.DirectionMain>
+          <DeclarationList />
+        </S.DirectionMain>
+      </S.Direction>
+    </S.Main>
+  );
+};
 export default Declaration;
