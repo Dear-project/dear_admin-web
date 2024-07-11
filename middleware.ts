0963a4b1a7f4429fe936e37f10f41d2f107e9b08
해준 <127281057+ftery0@users.noreply.github.com>
2024-07-12 01:48:54 +0900

    test push

--- middleware.ts
+++ middleware.ts
@@ -1,24 +1,26 @@
-import { NextRequest, NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from "next/server";
 
 export const config = {
-  matcher: ['/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)'],
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico|fonts|images).*)"],
 };
 
-const protectedRoutes = ['/']; // 로그인 정보가 있어야만 접근할 수 있는 페이지
-const publicRoutes = ['/login']; // 로그인 하지 않을 경우 접근할 수 있는 페이지
+const protectedRoutes = ["/"]; // 로그인 정보가 있어야만 접근할 수 있는 페이지
+const publicRoutes = ["/login"]; // 로그인 하지 않을 경우 접근할 수 있는 페이지
 
 export function middleware(request: NextRequest) {
-  const token = request.cookies.get('access-token');
+  const token = request.cookies.get("access-token");
   const currentPath = request.nextUrl.pathname;
 
   // 보호된 경로에 접근하려고 하지만 토큰이 없는 경우
-  if (!token && protectedRoutes.some((route) => currentPath.startsWith(route))) {
-   
-    return NextResponse.redirect(new URL('/login', request.url));
+  if (
+    !token &&
+    protectedRoutes.some((route) => currentPath.startsWith(route))
+  ) {
+    return NextResponse.redirect(new URL("/login", request.url));
   }
   // 공용 경로에 접근하려고 하지만 토큰이 있는 경우
   if (token && publicRoutes.some((route) => currentPath.startsWith(route))) {
-    return NextResponse.redirect(new URL('/', request.url));
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
   // 그 외의 경우
