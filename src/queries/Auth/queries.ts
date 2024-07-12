dcc0e498d5d48036c40f83365c8a89a32f7bbd98
해준 <127281057+ftery0@users.noreply.github.com>
2024-07-12 11:06:33 +0900

    remove :: 불필요 코드 삭제

--- src/queries/Auth/queries.ts
+++ src/queries/Auth/queries.ts
@@ -8,7 +8,3 @@ export const usePostLoginMutation = () => {
   return mutation;
 };
 
-// export const usePostSignupMutation = () => {
-//   const mutation = useMutation(AuthRepositoryImpl.signupStudent);
-//   return mutation;
-// };
